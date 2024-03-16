573a1db1a260c3ab655f074a697986763af7cf66
Gregory ARTAUD <gregory@galadrim.fr>
2024-03-16 21:30:12 +0100

    feat: added tep one route

--- apps/ui/src/router/routeDefinitions.tsx
+++ apps/ui/src/router/routeDefinitions.tsx
@@ -1,1 +1,5 @@
-export const ROUTE_PATH_TO_ELEMENT: Record<string, JSX.Element> = {};
+import { StepOne } from '../pages/steps/one/StepOne';
+
+export const ROUTE_PATH_TO_ELEMENT: Record<string, JSX.Element> = {
+  '00109116645f89f3a53977f33dde848a': <StepOne />,
+};
