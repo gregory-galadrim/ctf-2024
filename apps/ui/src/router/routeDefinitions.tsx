2147cdf55e2006b8bad9ff7dd355fbf776ca8e91
Gregory ARTAUD <gregory.rtd@gmail.com>
2024-03-25 00:49:45 +0100

    feat: ui route

--- apps/ui/src/router/routeDefinitions.tsx
+++ apps/ui/src/router/routeDefinitions.tsx
@@ -1,8 +1,10 @@
 import { STEP_IDENTIFIERS, type StepIdentifier } from 'steps';
+import { NotFoundPage } from '../pages/NotFound';
 import { StepTwoPage } from '../pages/steps/StepTwo.tsx/StepTwo';
 import { StepOnePage } from '../pages/steps/one/StepOne';
 
 export const ROUTE_PATH_TO_ELEMENT: Record<StepIdentifier, JSX.Element> = {
   [STEP_IDENTIFIERS.One]: <StepOnePage />,
   [STEP_IDENTIFIERS.Two]: <StepTwoPage />,
+  [STEP_IDENTIFIERS.Three]: <NotFoundPage />,
 };
